3f3877825f298ad5520e3106f400b327a8e97a95
Andrés <andres.diaz.alfonso@ieslaarboleda.es>
2026-01-15 20:10:16 +0100

    Add files via upload

---
+++ ej1.php
@@ -0,0 +1,10 @@
+<?php
+// Rellena un array con 10 enteros aleatorios y los muestra separados por comas
+$num = [];
+for ($i = 0; $i < 10; $i++) {
+	$num[] = rand(0, 100);
+}
+
+echo implode(',', $num);
+
+?>
