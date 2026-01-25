c3ed3d40c7b948cf9cab346ce62cd5eb256e42a4
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-01-25 19:49:08 +0100

    Footer

--- partials/footer.php
+++ partials/footer.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 fixed bottom-0">
+    <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 fixed bottom-0 bg-[#533d2d]">
   <aside>
     <svg
       width="50"
@@ -27,6 +27,7 @@
     </p>
   </aside>
   <nav>
+
     <h6 class="footer-title">Social</h6>
     <div class="grid grid-flow-col gap-4">
       <a>
@@ -58,7 +59,7 @@
           height="24"
           viewBox="0 0 24 24"
           a
-          class="fill-current">
+          class="fill-cur">
           <path
             d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
         </svg>
