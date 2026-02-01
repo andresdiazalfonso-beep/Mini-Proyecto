3291fd88383b6e8e40fb2138c800081b36d22c48
Abel <abel.hernandez.pereira@ieslaarboleda.es>
2026-02-01 20:32:03 +0100

    Productos Modificados 01/02/2026

---
+++ pages/producto.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="flex flex-wrap gap-6 justify-center p-10 bg-gray-100">
+
+
+<div class="flex flex-wrap gap-6 justify-center">
+    <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      class="h-60 object-cover p-4"
+      src="https://i.blogs.es/f1e62f/botella-agua/1366_2000.webp"
+      alt="Agua" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Agua Mineral
+      <div class="badge badge-primary text-lg font-bold"><em>500ml</em></div>
+    </h2>
+    <p>Una simple botella de agua.<br>Ayuda, aunque no lo parezca.</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.frigicoll.es/blog/wp-content/uploads/2019/09/las-lentejas-se-ponen-en-remojo.jpg"
+      alt="Lentejas" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Lentejas
+      <div class="badge badge-accent text-lg text-gray-800 font-bold"><em>450g</em></div>
+    </h2>
+    <p>Unas simples lentejas caseras.<br>Un alimento muy completo.</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1.50€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://okdiario.com/img/2022/02/19/palmeras-en-espana-655x368.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Semillas de Palmera
+      <div class="badge badge-success text-lg font-bold"><em>2 semillas</em></div>
+    </h2>
+    <p>Un par de semillas de palmera.<br>Con algo de agua y tiempo, proporcionan sombra y enriquecen la tierra.</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.eduardolozano.org/wp-content/uploads/2023/05/en-construccion.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+</div>
+<div class="flex flex-wrap gap-6 justify-center">
+    <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.eduardolozano.org/wp-content/uploads/2023/05/en-construccion.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.eduardolozano.org/wp-content/uploads/2023/05/en-construccion.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.eduardolozano.org/wp-content/uploads/2023/05/en-construccion.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+<div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://www.eduardolozano.org/wp-content/uploads/2023/05/en-construccion.jpg"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <a
+      href="#"
+      title="Confirmar donación"
+      class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono"><p class="font-bold text-shadow-lg">1€</p></a>
+    </div>
+  </div>
+</div>
+</div>
+   
+</body>
+</html>
