9b662780a792e2aa9305e60b14c526c48861acf7
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-06 08:30:42 +0100

    Idioma

--- language/lenguage.php
+++ language/lenguage.php
@@ -1,15 +1,7 @@
 <?php
-// Idiomas permitidos
-$idiomasPermitidos = ['es', 'en'];
-
 // Guardar idioma desde el select
-if (isset($_POST['idioma']) && in_array($_POST['idioma'], $idiomasPermitidos)) {
-    setcookie(
-        'idioma',
-        $_POST['idioma'],
-        time() + (3600 * 24 * 30),
-        '/'
-    );
+if (isset($_POST['idioma'])) {
+    setcookie('idioma', $_POST['idioma'], time() + 3600 * 24 * 30);
     header("Location: " . $_SERVER['REQUEST_URI']);
     exit();
 }
@@ -17,11 +9,6 @@ if (isset($_POST['idioma']) && in_array($_POST['idioma'], $idiomasPermitidos)) {
 // Idioma por defecto
 $idioma = $_COOKIE['idioma'] ?? 'es';
 
-// Validar cookie
-if (!in_array($idioma, $idiomasPermitidos)) {
-    $idioma = 'es';
-}
-
-// Cargar archivo de idioma
-$textos = include_once __DIR__ . "/$idioma.php";
+// Cargar idioma
+$textos = include_once "$idioma.php";
 ?>
