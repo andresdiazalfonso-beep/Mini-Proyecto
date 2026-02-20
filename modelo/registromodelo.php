7452a853076e19a937f73be506b8eea8644f5c79
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-20 13:30:21 +0100

    Registro Controlador

--- modelo/registromodelo.php
+++ modelo/registromodelo.php
@@ -3,7 +3,7 @@ function sanear($datos){
     return htmlspecialchars(trim($datos));
 }
 
-function validar_datos($datos,$conexion){
+function validar_datos($datos, $conexion){
     $errores = [];
 
     if(empty($datos['nombre']) || empty($datos['email'])  || empty($datos['password']) || empty($datos['password_confirm'])){
@@ -11,7 +11,7 @@ function validar_datos($datos,$conexion){
     }
 
     if(strlen($datos['nombre']) < 4){
-        $errores[] = "El nombre debe tener almenos 4 caracteres";
+        $errores[] = "El nombre debe tener al menos 4 caracteres";
     }
 
     if(!filter_var($datos['email'],FILTER_VALIDATE_EMAIL)){
@@ -19,45 +19,51 @@ function validar_datos($datos,$conexion){
     }
 
     if(strlen($datos['password']) < 6){
-        $errores[] = "La contraseña debe tener almenos 6 caracteres";
+        $errores[] = "La contraseña debe tener al menos 6 caracteres";
     }
-
 
     if($datos['password'] !== $datos['password_confirm']){
         $errores[] = "Las contraseñas no coinciden";
     }
 
-    //validar si existe email en la base de datos
+    //Validar si existe ya el email
     $sql = "SELECT id FROM usuarios WHERE email = ?";
-    $resultado = mysqli_prepare($conexion,$sql);
-    
-    if($resultado){
-        mysqli_stmt_bind_param($resultado,"s",$datos['email']);
-        mysqli_execute($resultado);
-        mysqli_store_result($resultado);
+    $stmt = mysqli_prepare($conexion, $sql);
 
-        if(mysqli_stmt_num_rows($resultado) > 0){
-            $errores[] = "El correo ya esta registrado";
+    if($stmt){
+        mysqli_stmt_bind_param($stmt, "s", $datos['email']);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+
+        if(mysqli_stmt_num_rows($stmt) > 0){
+            $errores[] = "El correo ya está registrado";
         }
-    }
-    mysqli_stmt_close($resultado);
 
+        mysqli_stmt_close($stmt);
+    }
 
     return $errores;
 }
 
-function guardar_registro($conexion,$datos){
-    $sql = "INSERT INTO usuarios (nombre,email,password) VALUES (?,?,?)";
-    $resultado = mysqli_prepare($sql);
-    mysqli_stmt_bind_param($resultado,"sss",$datos['nombre'],$datos['email'],$datos['password']);
 
-    if(mysqli_execute($resultado)){
-        mysqli_stmt_close($resultado);
-        return true;
-    }else{
-        mysqli_stmt_close($resultado);
+function guardar_registro($conexion, $datos){
+    $sql = "INSERT INTO usuarios (nombre,email,password) VALUES (?,?,?)";
+    $stmt = mysqli_prepare($conexion, $sql);
+
+    if($stmt){
+        mysqli_stmt_bind_param($stmt, "sss", $datos['nombre'], $datos['email'], $datos['password']);
+
+        if(mysqli_stmt_execute($stmt)){
+            mysqli_stmt_close($stmt);
+            return true;
+        } else {
+            mysqli_stmt_close($stmt);
+            return false;
+        }
+    } else {
         return false;
     }
 }
+
 
 ?>
