383fd77380021dd0e980e22c1b9ce0795284dffc
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-20 12:47:20 +0100

    Registro Controlador

--- controlador/registrocontrolador.php
+++ controlador/registrocontrolador.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require_once "../Conexion/conexion.php";
 require_once "../modelo/registromodelo.php";
 
 if($_SERVER['REQUEST_METHOD'] === "POST"){
@@ -12,11 +13,24 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
         "password_confirm" => sanear($_POST[['password_confirm']])
     ];
 
-    $errores = validar($datos);
+    $errores = validar($datos,$conexion);
+    $_SESSION['errores'] = $errores;
 
     if(empty($errores)){
-        
+        $resultado = guardar_registro($conexion,$datos);
+
+        if($resultado){
+            $_SESSION['mensaje'] = "Registro Correcto";
+            header("Location: ../pages/registro.php");
+            exit();
+        }else{
+            $_SESSION['errores'][] = "Error al registrar al usuario";
+            header("Location: ../pages/registro.php");
+            exit();
+        }
     }
 }
+header("Location: ../pages/registro.php");
+exit();
 
 ?>
