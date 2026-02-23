10eb43f13b24120f06c3a8ce7d77a851850c81f3
unknown <abel.hernandez.pereira@ieslaarboleda.es>
2026-02-23 10:59:36 +0100

    23/02/2026

--- pages/dinero.php
+++ pages/dinero.php
@@ -7,53 +7,57 @@ require_once '..\partials\header.php'; // Incluye el header fijo
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Dinero</title>
 </head>
-<body>
+<body class="mt-25">
 
-<div class="card card-side bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      alt="Movie" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Watch</button>
+<div class="flex flex-row  gap-4">
+    <div class="flex-col card card-side bg-base-100 shadow-sm">
+    <figure>
+        <img
+        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+        alt="Movie" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+        </div>
     </div>
-  </div>
-</div>
+    </div>
 
-<div class="card card-side bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      alt="Movie" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Watch</button>
+    <div class="flex-col card card-side bg-base-100 shadow-sm">
+    <figure>
+        <img
+        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+        alt="Movie" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+        </div>
     </div>
-  </div>
-</div>
+    </div>
 
-<div class="card card-side bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      alt="Movie" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Watch</button>
+    <div class="flex-col card card-side bg-base-100 shadow-sm">
+    <figure>
+        <img
+        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+        alt="Movie" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+        </div>
     </div>
-  </div>
+    </div>
 </div>
 
 
