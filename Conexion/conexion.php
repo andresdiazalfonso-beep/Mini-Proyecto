711e70bf450717247b771bfa7074e37c2c1224a7
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-23 10:51:16 +0100

    Conexion

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -1,4 +1,18 @@
 <?php
-$conexion = mysqli_connect("localhost","root","","help4africa");
+$basedatos = mysqli_connect("localhost","root","",);
+
+$verificar_basedatos = "SHOW DATABASE LIKE 'help4africa'";
+$resultado = mysqli_query($basedatos,$verificar_basedatos);
+
+if(mysqli_num_rows($resultado) == 0){
+    $crear_basedatos = "CREATE DATABASE help4afica";
+    mysqli_query($basedatos,$crear_basedatos);
+    $conexion = mysqli_connect("localhost","root","","help4africa");
+}
+
+
+
+
+
 
 ?>
