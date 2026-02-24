4733beb9d36b326b74f2d8df1021a92a23a78ee5
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-24 11:12:30 +0100

    Modelo Login

--- modelo/loginmodelo.php
+++ modelo/loginmodelo.php
@@ -1,6 +1,10 @@
 <?php
 require_once "../Conexion/conexion.php";
 
+function sanear($datos){
+    return htmlspecialchars(trim($datos));
+}
+
 function buscar_email($email){
     $sql = "SELECT 1 FROM usuarios WHERE email = ? LIMIT 1";
     $stmt = mysqli_prepare($conexion,$sql);
@@ -8,7 +12,13 @@ function buscar_email($email){
     if($stmt){
         mysqli_stmt_bind_param($stmt,"s",$email);
         mysqli_stmt_execute($stmt);
-        
+        mysqli_stmt_store_result($stmt);
+
+        $existe = mysqli_stmt_num_rows($stmt) > 0;
+
+        mysqli_stmt_close($stmt);
+
+        return $existe;
     }
 
 }
