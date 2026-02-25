fe0c180919d4e8a32f6fe2a566ce738e7cf1e26c
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-02-25 12:01:31 +0100

    Pie de pagina

--- partials/footer.php
+++ partials/footer.php
@@ -3,11 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
-<body>
+<body class="font-[Poppins]">
 
 <footer class="bg-[#5A4032] text-[#EAD9C9] w-full relative z-[1000] pt-5 mt-20">
 
