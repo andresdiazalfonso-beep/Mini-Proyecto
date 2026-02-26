1ebcf3d6a44019ee710e4f70ac4205b81c0ab31b
unknown <abel.hernandez.pereira@ieslaarboleda.es>
2026-02-26 09:39:17 +0100

    26/02/2026

--- pages/dinero.php
+++ pages/dinero.php
@@ -14,18 +14,32 @@ require_once '..\partials\header.php'; // Incluye el header fijo
     <style>
         html, body {
         background: linear-gradient(to bottom right, rgb(255, 238, 204), rgb(252, 168, 0), rgb(120, 79, 0));
-    }
+        }
+        button{
+        font-size: 20px;
+        }
     </style>
     <title>Dinero</title>
 </head>
 <body class="min-h-screen pt-30 flex justify-center font-[Poppins]">
-<div class="h-100 w-200 bg-white rounded-xl shadow-2xl hover:-translate-y-2 transition-transform p-10">
-    <button class="btn border-2 border-gray-800">5€</button>
-    <button class="btn border-2 border-gray-800">10€</button>
-    <button class="btn border-2 border-gray-800">25€</button>
-    <button class="btn border-2 border-gray-800">50€</button>
-    <button class="btn border-2 border-gray-800">75€</button>
-    <button class="btn border-2 border-gray-800">100€</button>
+<div class="h-100 w-200 bg-white rounded-xl shadow-2xl hover:-translate-y-2 transition-transform px-10 py-10 flex flex-col gap-3">
+    <div class="text-center font-bold text-orange-600 text-xl mb-8">
+        <div class="hover:cursor-default">Escoja una <span class="text-orange-400">opci&oacute;n</span> a pagar</div>
+    </div>
+    <div class="flex justify-around mx-25 gap-5">
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">5€</button>
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">10€</button>
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">25€</button>
+    </div>
+    <div class="flex justify-around mx-25 gap-5">
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">50€</button>
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">75€</button>
+        <button class="btn border-2 border-gray-800 py-6 flex-grow-1 hover:shadow-xl">100€</button>
+    </div>
+    <div class="flex justify-between mx-25 my-20 gap-5">
+        <button class="btn">Confirmar compra</button>
+        <a href="index.php" class="hover:cursor-pointer hover:underline text-orange-600 hover:font-bold">Volver al inicio</a>
+    </div>
 </div>
 
 
--- pages/producto.php
+++ pages/producto.php
@@ -10,21 +10,20 @@ require_once '..\partials\header.php'; // Incluye el header fijo
     <title>Productos</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <style>
+      html, body {
+      background: linear-gradient(to bottom right, rgb(255, 180, 89), rgb(252, 168, 0), rgb(120, 79, 0));
+      }
+    </style>
 </head>
 <body class="gap-6 justify-center p-10 bg-gray-100 mt-15">
-<div>
+<div class="hover:cursor-default">
   <div class="flex flex-wrap gap-6 justify-center">
     <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      class="h-65 object-cover p-4"
-      src="https://i.blogs.es/f1e62f/botella-agua/1366_2000.webp"
-      alt="Agua" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>500ml</em></div>
     <h2 class="card-title flex justify-between">
       Agua Mineral
-      <div class="badge badge-primary text-lg font-bold"><em>500ml</em></div>
     </h2>
     <p>Una simple botella de agua.<br>Ayuda, aunque no lo parezca.</p>
     <div class="card-actions justify-end">
@@ -32,24 +31,18 @@ require_once '..\partials\header.php'; // Incluye el header fijo
       href="#"
       title="Confirmar donación"
       class="hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all duration-300 shadow-md shadow-gray-500 badge text-2xl bg-gray-200 px-5 py-4 border-2 border-orange-300 font-mono">
-      <p class="font-bold text-shadow-lg">0.50€</p>
+      <p class="font-bold text-shadow-md">0.50€</p>
       </a>
     </div>
   </div>
 </div>
 <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      class="h-65 w-full object-cover"
-      src="https://mercadolasaguilas.es/wp-content/uploads/2025/04/Diseno-sin-titulo-9-782x1024.jpg"
-      alt="Comida" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>0.5kg</em></div>
     <h2 class="card-title flex justify-between">
-      Comida variada
-      <div class="badge badge-info text-lg text-gray-800 font-bold"><em>0.5kg</em></div>
+      Pack de alimentos
     </h2>
-    <p>Una variedad de alimentos.<br>Conviene tener una dieta equilibrada.<br>Almuerzo y cena para 2.</p>
+    <p>Una variedad de alimentos.<br>Almuerzo y cena para 2.</p>
     <div class="card-actions justify-end">
       <a
       href="#"
@@ -61,18 +54,12 @@ require_once '..\partials\header.php'; // Incluye el header fijo
   </div>
 </div>
 <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-    class="h-65 w-full"
-      src="https://okdiario.com/img/2022/02/19/palmeras-en-espana-655x368.jpg"
-      alt="Palmera" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>Botiqu&iacute;n</em></div>
     <h2 class="card-title flex justify-between">
-      &Aacute;rboles
-      <div class="badge badge-success text-lg font-bold"><em>20 semillas</em></div>
+      Kit m&eacute;dico
     </h2>
-    <p>Semillas para clima &aacute;rido<br>Con algo de agua y tiempo, proporcionan sombra y enriquecen la tierra.</p>
+    <p>Para curar heridas y enfermedades leves.<br>Para ni&ntilde;os y adultos.</p>
     <div class="card-actions justify-end">
       <a
       href="#"
@@ -84,18 +71,12 @@ require_once '..\partials\header.php'; // Incluye el header fijo
   </div>
 </div>
 <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-    class="h-65 w-full"
-      src="https://www.pequerecetas.com/wp-content/uploads/2009/02/pures-para-bebes-de-6-meses.jpg"
-      alt="Papillas" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>2 comidas</em></div>
     <h2 class="card-title flex justify-between">
-      Papillas
-      <div class="badge badge-secondary text-lg font-bold"><em>2 botes</em></div>
+      Comida infantil
     </h2>
-    <p>Para los ni&ntilde;os.<br>Contienen vitaminas de diferentes frutas, correctas para su desarrollo.</p>
+    <p>Para los ni&ntilde;os.<br>Contienen vitaminas de diferente tipo, correctas para su desarrollo.</p>
     <div class="card-actions justify-end">
       <a
       href="#"
@@ -108,16 +89,10 @@ require_once '..\partials\header.php'; // Incluye el header fijo
 </div>
 <div class="flex flex-wrap gap-6 justify-center">
     <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-    class="h-65 w-full"
-      src="https://jardinesycampos.es/502-large_default/kit-caja-de-herramienta.jpg"
-      alt="Herramientas de cultivo" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>5 piezas</em></div>
     <h2 class="card-title flex justify-between">
       Equipo
-      <div class="badge badge-secondary text-lg font-bold"><em>5 piezas</em></div>
     </h2>
     <p>Sombrero, guantes, palas de 2 tama&ntilde;os y una hazada.</p>
     <div class="card-actions justify-end">
@@ -132,18 +107,12 @@ require_once '..\partials\header.php'; // Incluye el header fijo
 </div>
 <div class="flex gap-6 justify-center">
     <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      class="h-65 object-cover"
-      src="https://www.agroboca.com/uploads/blog/produccion-artesanal-de-semillas.jpg"
-      alt="Semillas" />
-  </figure>
   <div class="card-body">
+    <div class="badge rounded-full bg-orange-100 text-sm font-bold text-orange-600"><em>30 semillas</em></div>
     <h2 class="card-title flex justify-between">
-      Semillas de hortaliza
-      <div class="badge badge-secondary text-lg font-bold"><em>30 semillas</em></div>
+      Semillas
     </h2>
-    <p>Algunas semillas para hortalizas de diverso tipo.<br>Pueden soprtar condiciones clim&aacute;ticas adversas.</p>
+    <p>Algunas semillas de diverso tipo.<br>Pueden soprtar condiciones clim&aacute;ticas adversas.</p>
     <div class="card-actions justify-end">
       <a
       href="#"
@@ -157,10 +126,10 @@ require_once '..\partials\header.php'; // Incluye el header fijo
 </div>
 </div>
 
-<div class="flex flex-row g-10 text-center">
+<div class="flex flex-row g-10 text-center text-gray-50">
   <p>Todo donativo realizado est&aacute; 100% destinado a la ayuda y mantenimiento de la vida sudafricana, 
     agradecemos su ayuda tanto el equipo dedicado a esto, como las cientas de personas salvadas diariamente 
-    gracias a las donaciones que nos llegan y le podemos hacer llegar.<br>Gracias de todo corazón.
+    gracias a las donaciones que nos llegan y les podemos hacer llegar.<br>Gracias de todo coraz&oacute;n.
   </p>
 </div>
    
