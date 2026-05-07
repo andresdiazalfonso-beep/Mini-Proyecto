ef7e52bdfe613a0c4343c295403dc450405e6801
unknown <andres.diaz.alfonso@ieslaarboleda.es>
2026-05-07 11:25:33 +0200

    Pasarela de pago Tienda Producto

--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\Stripe\Stripe::setApiKey(getenv("sk_test_51TTdWuKBvC3TK4asje46PGs2pdcqKpQNFxndCDTUYTwADnMIx0wN6dajZ7Ta1cKn3kq9v0RvdlCn1JTanBNwW6Mq00T0o7Hmlu"));
+\Stripe\Stripe::setApiKey("CODIGO_API_PRIVADA");
 
 ?>
