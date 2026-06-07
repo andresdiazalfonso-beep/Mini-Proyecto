fae20a44df33ad3bae9300eac8ea61e4dfea0477
Abel <abel.hernandez.pereira@ieslaarboleda.es>
2026-06-07 17:05:15 +0200

    07/06/2026 Modificaciones leves --Abel--

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if (isset($_SESSION['usuario'])) {
+    header("Location: ../pages/usuario.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- partials/logout.php
+++ partials/logout.php
@@ -2,6 +2,8 @@
 session_start();
 $_SESSION = [];
 session_destroy();
+setcookie("recordar_email","", time() - 3600, "/");
+setcookie("recordar_password", "", time() - 3600, "/");
 header("Location: ../pages/index.php");
 exit();
 ?>
